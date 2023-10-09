8af5f76d3b459ea822b6845824a9163ad3e455d4
ApprovedNPC <rockyw2026@gmail.com>
2023-10-09 18:19:00 +1100

    Added more customisation for NavBar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
-import {
-  Button,
-  Flex,
-  Text,
-  useColorMode,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Flex, Text, Image } from "@chakra-ui/react";
 import { NavBar } from "./components/NavBar";
 
 function Stat() {
   return (
     <>
       <Text>Distance</Text>
-      <Text>Distance</Text>
     </>
   );
 }
 
 export default function App() {
-  const { toggleColorMode } = useColorMode();
-  const toggleText = useColorModeValue("dark", "light");
-
   return (
     <Flex w="full" direction={"column"} align="center" gap={"10px"}>
       <NavBar />
-      <Button onClick={toggleColorMode}>Switch to {toggleText} mode</Button>
+
       <Stat />
+      <Image
+        src="https://i.stack.imgur.com/wIPlH.jpg"
+        alt="Map of 16 Kensington Rd, Kensington"
+        width="50%"
+        rounded="lg"
+      />
     </Flex>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,17 @@
 import {
   Button,
+  Avatar,
   Flex,
   Heading,
   Spacer,
   useColorModeValue,
+  useColorMode,
 } from "@chakra-ui/react";
 
 export function NavBar() {
   const navColor = useColorModeValue("gray.100", "gray.900");
+  const { toggleColorMode } = useColorMode();
+  const toggleText = useColorModeValue("dark", "light");
 
   return (
     <Flex
@@ -23,6 +27,10 @@ export function NavBar() {
       <Spacer />
       <Button colorScheme="teal">Notifications</Button>
       <Button colorScheme="orange">Log in</Button>
+      <Button onClick={toggleColorMode} colorScheme="pink">
+        Switch to {toggleText} mode
+      </Button>
+      <Avatar name="Rocky Wu" src="" />
     </Flex>
   );
 }
