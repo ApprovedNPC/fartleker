d94766765695a3a64c723d377516452d64c48b2d
ApprovedNPC <rockyw2026@gmail.com>
2023-10-13 11:18:54 +1100

    Hamzah told me to commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,19 @@ export default function App() {
   return (
     <Flex w="full" direction={"column"} align="center" gap={"10px"}>
       <NavBar />
+      <Flex direction="row" gap="10px">
+        <Flex>A</Flex>
+        <Flex>A</Flex>
+        <Flex>A</Flex>
+      </Flex>
 
-      <Stat />
+      {/* <Stat />
       <Image
         src="https://i.stack.imgur.com/wIPlH.jpg"
-        alt="Map of 16 Kensington Rd, Kensington"
+        alt="Prototype map"
         width="50%"
         rounded="lg"
-      />
+      /> */}
     </Flex>
   );
 }
